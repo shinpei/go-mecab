2fd2df4db540a77a15624f85f3aed7f8fca8eb00
shinpei <shinpei.nakata@gmail.com>
2014-04-02 00:53:28 +0900

    cannot use yet

--- example.go
+++ example.go
@@ -5,14 +5,25 @@ package main
 #include <mecab.h>
 
 static mecab_t * InitMecab(void) {
-  mecab_t *m = mecab_new2("");
-  return m;
+    char *dummy = "";
+    mecab_t *m = mecab_new2(dummy);
+    return m;
 }
 
-*/
-
+static const char * ParseMecab(mecab_t *mecab, const char *input) {
+    const char *ret = mecab_sparse_tostr(mecab, input);
+    return ret;
+}
+ */
 import "C"
 
+import "fmt"
+
 func main () {
-  var m *C.mecab_t = C.InitMecab();
+	var m *C.mecab_t = C.InitMecab();
+	var str = "Hoge Hoge";
+	p := C.ParseMecab(m, C.CString(str));
+	s := C.GoString(p);
+	fmt.Printf("str: '%s'\n" , s);
+	
 }
